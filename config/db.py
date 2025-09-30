2e91953d60917dfb64de8a8dd050b3f263cff642
prakirtibista <158180233+prakirtibista@users.noreply.github.com>
2025-10-01 02:13:45 +0530

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-mongo_uri = "mongodb+srv://prakirtibista7:Bunuchhetry2@cluster0.egseduj.mongodb.net/"
+mongo_uri = "mongodb+srv://<username:pw>@cluster0.egseduj.mongodb.net/"
 conn = MongoClient(mongo_uri)
-db = conn["notes"]
+db = conn["<db name>"]
